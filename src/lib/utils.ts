7f1d8362751d7faff1cbf581a5a66af9ebbeed05
Mickey1996 <mk.bythesea1996@gmail.com>
2025-08-08 18:52:48 +0900

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,6 @@
 import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-variants"
+import { twMerge } from "tailwind-merge"; 
+
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
